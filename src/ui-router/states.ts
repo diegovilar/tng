34edf4a2266869b253f3e07ca8494e7d7884c671
Diego Vilar <diegovilar@gmail.com>
2015-07-31 17:56:16 -0300

    Upgraded angular and angular-mocks to ~1.4.0; Upgraded reflect-metadata to ~0.1.0

--- src/ui-router/states.ts
+++ src/ui-router/states.ts
@@ -16,21 +16,21 @@ export {StateChangeEvent, ViewLoadEvent} from './events';
  * TODO document
  */
 export interface StateConfig {
-    
+
     url: string;
     abstract?: boolean;
     view?: Function;
     views?: {[outlet:string]: Function};
     parent?: StateConfig|string;
-    
+    reloadOnSearch?: boolean;
+    onEnter?: Function;
+	onExit?: Function;
+
     // TODO
     // params
     // resolve
     // data
-    // reloadOnSearch
-    // onEnter
-    // onExit
-    
+
 }
 
 /**
@@ -44,14 +44,14 @@ export interface InternalStateConfig extends StateConfig {
  * @internal
  */
 export class StatesAnnotation {
-    
+
     states: Map<InternalStateConfig>;
 
     constructor(states: Map<InternalStateConfig>) {
-        
+
         forEach(states, (state, name) => state.name = name);
         this.states = states;
-        
+
     }
 
 }
@@ -71,15 +71,15 @@ States.on = On;
  * @internal
  */
 export function publishStates(moduleController: Function, ngModule: ng.IModule) {
-    
+
     // Reflect.decorate apply decorators reversely, so we need to reverse
     // the extracted annotations to ge them on the original order
     var statesAnnotation = <StatesAnnotation[]> getAnnotations(moduleController, StatesAnnotation).reverse();
-        
+
     if (statesAnnotation.length) {
         let states: ng.ui.IState[] = [];
 
-        // Translate each state from each annotation and stack them in an array        
+        // Translate each state from each annotation and stack them in an array
         forEach(statesAnnotation, (note) =>
             forEach(note.states, (state) =>
                 states.push(translateToUiState(state))));
@@ -92,22 +92,25 @@ export function publishStates(moduleController: Function, ngModule: ng.IModule) 
 
         }));
     }
-    
+
     publishListeners(moduleController, ngModule);
-    
+
 }
 
 /**
  * @internal
  */
 function translateToUiState(state: InternalStateConfig): ng.ui.IState {
-    
+
     var translatedState:ng.ui.IState = {};
-    
+
     if (isDefined(state.name)) translatedState.name = state.name;
     if (isDefined(state.url)) translatedState.url = state.url;
     if (isDefined(state.abstract)) translatedState.abstract = state.abstract;
-    
+    if (isDefined(state.reloadOnSearch)) translatedState.reloadOnSearch = state.reloadOnSearch;
+    if (isDefined(state.onEnter)) translatedState.onEnter = state.onEnter;
+    if (isDefined(state.onExit)) translatedState.onExit = state.onExit;
+
     // If the state has a parent, we force the string way
     if (isDefined(state.parent)) {
         let parent = state.parent;
@@ -117,7 +120,7 @@ function translateToUiState(state: InternalStateConfig): ng.ui.IState {
         // ng.ui.IState is missing parent
         (<any> translatedState).parent = parent;
     }
-        
+
     if (state.view && state.views) {
         throw Error('Cannot provide both view and views');
     }
@@ -126,15 +129,15 @@ function translateToUiState(state: InternalStateConfig): ng.ui.IState {
     }
     else {
         let views = <{[key:string]:any}> {};
-        
+
         if (state.view) {
             views[''] = extractViewData(state.view);
         }
         else {
             forEach(state.views, (controller, outlet) => views[outlet] = extractViewData(controller))
         }
-        
-        translatedState.views = views;    
+
+        translatedState.views = views;
     }
 
     return translatedState;
@@ -145,24 +148,24 @@ function translateToUiState(state: InternalStateConfig): ng.ui.IState {
  * @internal
  */
 function extractViewData(viewModel: Function) {
-    
+
     // Reflect.decorate apply decorators reversely, so we need to reverse
     // the extracted annotations before merging them
     let notes = getAnnotations(viewModel, ViewAnnotation).reverse();
-    
+
     if (!notes.length) {
         throw new Error('Template not defined');
     }
-    
+
     let template = <ViewAnnotation> mergeAnnotations({}, ...notes);
     let data:any = {};
-    
+
     data.controller = viewModel;
-    if (template.controllerAs) data.controllerAs = template.controllerAs;    
+    if (template.controllerAs) data.controllerAs = template.controllerAs;
     if (template.template) data.template = template.template;
     if (template.templateUrl) data.templateUrl = template.templateUrl;
     // TODO style?
-    
+
     return data;
-    
+
 }
