7bf1f3a35bf67aacb902d9182f58eeb4b9687989
Diego Vilar <diegovilar@gmail.com>
2016-11-17 09:41:18 -0300

    Work in progress

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,9 @@
-// export {Inject, injectable} from './di'
-// export {Value} from './value';
-// export {Constant} from './constant';
-// export {Filter} from './filter';
-// export {Animation} from './animation';
-// export {Service} from './service';
-// export {Decorator} from './decorator';
-// export {View} from './view';
-// export {ComponentView, ComponentTemplateNamespace} from './component-view';
-// export {Directive, Transclusion, Bind} from './directive';
-// export {Component} from './component';
-// export {Module, publishModule} from './module';
-// export {Application} from './application';
-// export {bootstrap} from './bootstrap';
+export * from "./core/index";
 
-export * from "./animation";
-export * from "./application";
-export * from "./bootstrap";
-export * from "./component";
-export * from "./component-view";
-export * from "./constant";
-export * from "./decorator";
-export * from "./di";
-export * from "./directive";
-export * from "./filter";
-export * from "./module";
-export * from "./service";
-export * from "./value";
-export * from "./view";
+import * as uiBootstrap from "./ui.bootstrap/index";
+import * as uiRouter from "./ui.router/index";
+
+export let ui = {
+    bootstrap: uiBootstrap,
+    router: uiRouter
+};
---
+++ src/rollup.config.js
@@ -0,0 +1,33 @@
+let pak = require("../package.json");
+import sourcemaps from "rollup-plugin-sourcemaps";
+import commonjs from "rollup-plugin-commonjs";
+
+let banner = `/**
+ * @preserve
+ * AngularTS - ${pak.description}
+ * Version: ${pak.version} (built on ${(new Date()).toUTCString()})
+ * Project: ${pak.homepage}
+ * Author:  ${pak.author}
+ * License: ${pak.license}
+ */`;
+
+export default {
+    banner,
+    entry: "../build/angularts/index.js",
+    dest:  "../build/angularts/bundles/index.js",
+    format: "umd",
+    moduleName: "angularts",
+    exports: "named",
+    sourceMap: true,
+    globals: {
+        angular: "angular"
+    },
+    plugins: [
+        sourcemaps(),
+        commonjs({
+            exlude: [
+                "../node_modules/**"
+            ]
+        })
+    ]
+};
